190a1aa1ebc51e34d3fa5eb147198746e6e75373
adambouyadmar <adam.bouyadmar2006@gmail.com>
2026-07-24 13:05:32 +0100

    Initial dotfiles

---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,9 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+vim.cmd([[
+  highlight Normal guibg=NONE ctermbg=NONE
+  highlight NormalNC guibg=NONE ctermbg=NONE
+  highlight SignColumn guibg=NONE
+  highlight EndOfBuffer guibg=NONE
+]])
